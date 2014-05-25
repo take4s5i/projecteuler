fbc632cad871da454f9cb8a9540abfb951ee805a
take4s5i <take4s5i.0000@gmail.com>
2014-05-26 02:54:31 +0900

    solve problem2

---
+++ src/projecteuler/problem2.clj
@@ -0,0 +1,12 @@
+(ns projecteuler.problem2)
+
+(defn- fibonacci-nums []
+  (map second
+       (iterate (fn [[x y]] [y (+ x y)])
+                [1 1])))
+
+(defn solve []
+  (->> (fibonacci-nums)
+       (take-while #(< % 4000000))
+       (filter even?)
+       (reduce +)))
